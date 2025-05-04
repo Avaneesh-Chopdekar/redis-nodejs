9b3cad17e5f973f25d11bbdc8c5390b7e4e85b0b
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-05-04 22:22:56 +0530

    added logic of GET, SET, DEL, and EXPIRE

--- src/core.js
+++ src/core.js
@@ -2,6 +2,22 @@ import loggerFn from "./logger.js";
 
 const logger = loggerFn("core");
 
+const store = {};
+const expirationTimes = {};
+
+const isExpired = (key) =>
+  expirationTimes[key] && expirationTimes[key] < Date.now();
+
+const checkExpiration = (key) => {
+  if (isExpired(key)) {
+    delete store[key];
+    delete expirationTimes[key];
+
+    return true;
+  }
+  return false;
+};
+
 export const parseCommand = (data) => {
   if (!data) {
     logger.error("No data received");
@@ -23,13 +39,60 @@ const commandHandlers = {
   PING: () => "+PONG\r\n",
   COMMAND: () => "+OK\r\n",
   INFO: () => "+OK\r\n",
-  SET: () => {
-    // implementation for SET command
+  SET: (args) => {
+    if (args.length < 2) {
+      logger.error("SET command requires at least 2 arguments");
+      return "-ERR wrong number of arguments for 'set' command\r\n";
+    }
+    const key = args[0];
+    const value = args[1];
+    store[key] = { type: "string", value };
+
     return "+OK\r\n";
   },
-  GET: (key) => {
-    // implementation for GET command
-    return `$${key.length}\r\n${key}\r\n`;
+  GET: (args) => {
+    if (args.length < 1) {
+      return "-ERR wrong number of arguments for 'get' command\r\n";
+    }
+
+    const key = args[0];
+
+    if (checkExpiration(key) || !store[key] || store[key].type !== "string") {
+      return "$-1\r\n";
+    }
+
+    const value = store[key].value;
+
+    return `$${value.length}\r\n${value}\r\n`;
+  },
+  DEL: (args) => {
+    if (args.length < 1) {
+      return "-ERR wrong number of arguments for 'del' command\r\n";
+    }
+
+    const [key] = args;
+
+    if (store[key]) {
+      delete store[key];
+      delete expirationTimes[key];
+
+      return ":1\r\n";
+    } else {
+      return ":0\r\n";
+    }
+  },
+  EXPIRE: (args) => {
+    if (args.length < 2) {
+      return "-ERR wrong number of arguments for 'expire' command\r\n";
+    }
+
+    const [key, seconds] = args;
+
+    if (!store[key]) return ":0\r\n";
+
+    expirationTimes[key] = Date.now() + seconds * 1000;
+
+    return ":1\r\n";
   },
 };
 
