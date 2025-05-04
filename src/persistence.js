66d826e76a1ce17903975514603ac6daf940462a
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-05-05 00:51:55 +0530

    created Persistance class for storing and loading snapshots of data

---
+++ src/persistence.js
@@ -0,0 +1,58 @@
+import fs from "fs";
+import path from "path";
+
+import loggerFn from "./utils/logger.js";
+
+const logger = loggerFn("persistence");
+
+const fsp = fs.promises;
+
+class Persistence {
+  DATA_FILE = path.join(__dirname, "data.rdb");
+
+  constructor() {
+    if (Persistence.instance) {
+      return Persistence.instance;
+    }
+
+    Persistence.instance = this;
+
+    this.store = {};
+    this.expirationTimes = {};
+  }
+
+  async saveSnapshot() {
+    const data = JSON.stringify({
+      store: this.store,
+      expirationTimes: this.expirationTimes,
+    });
+
+    try {
+      await fsp.writeFile(this.DATA_FILE, data);
+      logger.info(`Snapshot saved to ${this.DATA_FILE}`);
+    } catch (error) {
+      logger.error(`Error saving snapshot: ${error.message}`);
+    }
+  }
+
+  loadSnapshotSync() {
+    if (!fs.existsSync(this.DATA_FILE)) return;
+
+    try {
+      const data = fs.readFileSync(this.DATA_FILE).toString();
+      if (!data) return;
+
+      const { store: loadedStore, expirationTimes: loadedExpirationTimes } =
+        JSON.parse(data);
+
+      Object.assign(this.store, loadedStore);
+      Object.assign(this.expirationTimes, loadedExpirationTimes);
+
+      logger.info(`Snapshot loaded from ${this.DATA_FILE}`);
+    } catch (error) {
+      logger.error(`Error loading snapshot: ${error.message}`);
+    }
+  }
+}
+
+export const persistence = new Persistence();
